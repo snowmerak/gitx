12bc925ffaf66b50e590b3b0ec67bcfdbcd478cf
snowmerak <snowmerak@outlook.com>
2024-01-27 16:14:11 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "time"
+
 func main() {
 	g, err := NewGit(".", nil)
 	if err != nil {
@@ -12,6 +14,24 @@ func main() {
 	}
 
 	if err := b.CheckoutToFeature("test"); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	if err := b.CheckoutToProposal("test"); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	if err := b.ReturnToPrevious(); err != nil {
+		panic(err)
+	}
+
+	time.Sleep(1 * time.Second)
+
+	if err := b.ReturnToPrevious(); err != nil {
 		panic(err)
 	}
 }
