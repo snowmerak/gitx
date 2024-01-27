5c5466ee51dc4918ca9b03da1dd3106f9e80dc3b
snowmerak <snowmerak@outlook.com>
2024-01-27 18:35:47 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,37 +1,7 @@
 package main
 
-import "time"
+import "github.com/alecthomas/kingpin/v2"
 
 func main() {
-	g, err := NewGit(".", nil)
-	if err != nil {
-		panic(err)
-	}
-
-	b, err := NewBranch(".", g)
-	if err != nil {
-		panic(err)
-	}
-
-	if err := b.CheckoutToFeature("test"); err != nil {
-		panic(err)
-	}
-
-	time.Sleep(1 * time.Second)
-
-	if err := b.CheckoutToProposal("test"); err != nil {
-		panic(err)
-	}
-
-	time.Sleep(1 * time.Second)
-
-	if err := b.ReturnToPrevious(); err != nil {
-		panic(err)
-	}
-
-	time.Sleep(1 * time.Second)
-
-	if err := b.ReturnToPrevious(); err != nil {
-		panic(err)
-	}
+	configCommand := kingpin.Command("config", "Manage configuration")
 }
