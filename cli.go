3f971b6bcfa9bb5f3ca5e3531e40d0b1e841437a
snowmerak <snowmerak@outlook.com>
2024-01-27 21:54:45 +0900

    add ssh generate command

--- cli.go
+++ cli.go
@@ -14,19 +14,28 @@ type Cli struct {
 
 		Init *kingpin.CmdClause
 	}
+
+	SSH struct {
+		Generate *kingpin.CmdClause
+	}
 }
 
 func NewCli() *Cli {
+	cli := &Cli{}
+
 	configCommand := kingpin.Command("config", "Manage configuration")
 
 	configGitignoreCommand := configCommand.Command("ignore", "Manage gitignore configuration")
-
-	cli := &Cli{}
 
 	cli.Config.Gitignore.Init = configGitignoreCommand.Command("init", "Initialize gitignore configuration").Action(configGitignoreInitAction)
 
 	cli.Config.Init = configCommand.Command("init", "Initialize configuration").Action(configInitAction)
 	configInitArgSshKey = cli.Config.Init.Arg("ssh-key", "SSH key to use").String()
+
+	sshCommand := kingpin.Command("ssh", "Manage SSH keys")
+
+	cli.SSH.Generate = sshCommand.Command("generate", "Generate SSH key").Action(sshGenerateAction)
+	sshGenerateArgName = cli.SSH.Generate.Arg("name", "Name of the SSH key").Required().String()
 
 	return cli
 }
